562136ae4e21ca6f140a44053fd640ce5cccdce6
Spencer Tuft <stuft2@byu.edu>
2021-10-26 16:00:46 -0600

    Add 32-byte test

--- checksum/test/index.spec.ts
+++ checksum/test/index.spec.ts
@@ -2,16 +2,16 @@ import { genCrc8Checksum } from '../src'
 
 function text2Binary(value: string) {
     return value.split('').map(char => {
-        return char.charCodeAt(0).toString(2).padStart(8, '0');
-    }).join(' ');
+        return char.charCodeAt(0).toString(2).padStart(8, '0')
+    }).join(' ')
 }
 
 function binaryToText(bin: string) {
-    //Convert the binary into an array of binary strings separated by whitespace.
-     const binary = bin.split(' ');
-    //convert from binary to decimals, then to characters. 
-    return binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
-    }
+    // Convert the binary into an array of binary strings separated by whitespace.
+     const binary = bin.split(' ')
+    // Convert from binary to decimals, then to characters. 
+    return binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("")
+}
 
 describe ('Generate CRC8 Checksum', () => {
     test('Test "hello world" error', () => {
@@ -63,5 +63,17 @@ describe ('Generate CRC8 Checksum', () => {
         const checksum = genCrc8Checksum(input)
 
         expect(checksum).toHaveLength(2)
+    })
+
+    test('Test 32-byte data stream with at least one error', () => {
+        const text = 'Lorem ipsum dolor sit amet, cons'
+        const input = text2Binary(text)
+        const error = input.replace('1', '0') // Change first "1" bit to "0"
+
+        const checksum = genCrc8Checksum(input)
+        const errChecksum = genCrc8Checksum(error)
+
+        expect(input.split(' ').length).toEqual(32) // Split input into byte chunks
+        expect(checksum).not.toEqual(errChecksum)
     })
 })
